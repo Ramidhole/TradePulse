d321f262e8ce27ddc71f6746097107fc326eb077
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-14 18:20:24 +0530

    add pricing component

--- frontend/src/landing_page/home/Priceing.js
+++ frontend/src/landing_page/home/Priceing.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 function Pricing() {
     return (  
-        <h1>Pricing</h1>
+
+        <div className='container'>
+            <div className='row text-center'>
+                <div className='col-4'>
+                    <h1 className='mb-3'>Unbeatable Pricig</h1>
+                    <p >We pioneered the concept of discount broking and price transparency in india. Flat fees and no Hdden changes.</p>
+                    <a href='' className='mx-5'>See pricing<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+
+                </div>
+                <div className='col-2'></div>
+
+                <div className='col-6'>
+                    <div className='row text-center'>
+                    <div className='col p-3 border'>
+
+                        <h1 className='mb-3'>&#8377; 0</h1>
+                        <p>Free equity delivery and <br/> direct mutual funds</p>
+                    </div>
+                    <div className='col p-3 border'>
+                        <h1 className='mb-3'>&#8377; 20 </h1>
+                        <p>Intraday F&O</p>
+                    </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
     );
 }
 
