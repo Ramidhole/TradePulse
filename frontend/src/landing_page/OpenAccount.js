e9709a51f263b7c083bd6f9bea5b267956c43a85
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-15 23:31:52 +0530

    add navbar component

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function OpenAccount() {
-    return ( 
-        <h1>OpenAccount</h1>
-     );
+    return (
+      <div className="container p-4 mb-5">
+        <div className="row text-center">
+          <h1 className="mt-5">Open an tradepulse account</h1>
+          <p>Modern platform and apps, &#8377; 0 investments and flat &#8377; 20 Intraday amd F&O trades.</p>
+          <button
+            className="p2 btn btn-primary mb-5"
+            style={{ width: "20%", margin: "0 auto" }}
+          >
+            Signup Now
+          </button>
+        </div>
+      </div>
+    )
 }
 
 export default OpenAccount;
