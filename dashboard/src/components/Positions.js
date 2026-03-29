231909d99a9af3580051e99867bf1b5a3efe43f5
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-29 19:24:59 +0530

    creae a basic dashboard

---
+++ dashboard/src/components/Positions.js
@@ -0,0 +1,26 @@
+import React, { Fragment , userEffect} from "react"
+
+
+const Positions = () => {
+  return (
+    <>
+      <h3 className="title">Position</h3>
+
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Product</th>
+            <th>Instrument</th>
+            <th>Qty.</th>
+            <th>Avg.</th>
+            <th>LTP</th>
+            <th>P&L</th>
+            <th>Chg.</th>
+          </tr>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default Positions
