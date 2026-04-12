200d0e3b4b71b135675a152c84f321bcbe703b89
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-04-12 23:22:39 +0530

    add chart and graph

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,5 +1,6 @@
 import React ,{useState ,useEffect} from 'react'
 import axios from "axios"
+import { VerticalGraph } from './VerticalGraph';
 
 
 import {holdings} from "../data/data"
@@ -17,6 +18,19 @@ const Holdings = () => {
     });
   },[]);
   
+ const labels = allHoldings.map((subArray) => subArray["name"])
+
+ const data = {
+   labels,
+   datasets: [
+     {
+       label: "Stock Price",
+       data: allHoldings.map((stock) => stock.price),
+       backgroundColor: "rgba(255, 99, 132, 0.5)",
+     },
+   ],
+ }
+
   return (
     <>
       <h3 className="title">Holdings({allHoldings.length})</h3>
@@ -77,6 +91,7 @@ const Holdings = () => {
           <p>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data} />
     </>
   )
 };
